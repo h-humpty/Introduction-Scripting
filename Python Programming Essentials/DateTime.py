6e938e7324aecf9b8c1091c822a2c1f01b7d639b
passionatedeveloper223 <strongwolf223@gmail.com>
2020-09-14 16:52:02 +0500

    Update DateTime.py

--- Python Programming Essentials/DateTime.py
+++ Python Programming Essentials/DateTime.py
@@ -12,7 +12,7 @@ def days_in_month(year, month):
     a = False
     if year % 4 == 0:
         a = True
-    if month in [1 , 3 , 5 , 7, 8, 10, 12]:
+    if month in [1, 3, 5, 7, 8, 10, 12]:
         return 31
     elif month == 2:
         if a == True:
@@ -33,10 +33,10 @@ def is_valid_date(year, month, day):
       False otherwise
     """
     total_days = days_in_month(year, month)
-    if day == total_days:
-        return True
-    else:
+    if day > total_days or day <=0 or month > 12 or month <=0 or year <= 0:
         return False
+    else:
+        return True
      
 def days_between(year1, month1, day1, year2, month2, day2):
     """
@@ -53,9 +53,9 @@ def days_between(year1, month1, day1, year2, month2, day2):
       Returns 0 if either date is invalid or the second date is 
       before the first date.
     """
-    if is_valid_date(year1, month1, day1) or is_valid_date(year2, month2, day2):
+    if not (is_valid_date(year1, month1, day1)) or not(is_valid_date(year2, month2, day2)):
         return 0
-    if year2 < year1 or (year1==year2 and month2 < month1) or   (year1==year2 and month2 == month1 and day2 < day1):
+    if year2 < year1 or (year1 == year2 and month2 < month1) or   (year1 == year2 and month2 == month1 and day2 < day1):
         return 0
     total_days = 0
     
@@ -67,8 +67,8 @@ def days_between(year1, month1, day1, year2, month2, day2):
         total_days += (day_temp - day1)
         total_days += day2
         while temp < month2:
-            total_days += days_in_month(year1 , temp)
-            temp+=1
+            total_days += days_in_month(year1, temp)
+            temp += 1
         return total_days
     else:
         temp = month1 + 1
@@ -80,8 +80,8 @@ def days_between(year1, month1, day1, year2, month2, day2):
             temp = 1
             year1 += 1
         while temp != month2 or year1 != year2:
-            total_days += days_in_month(year1 , temp)
-            temp+=1
+            total_days += days_in_month(year1, temp)
+            temp += 1
             if temp == 13:
                 temp = 1
                 year1 += 1
@@ -99,13 +99,15 @@ def age_in_days(year, month, day):
       Returns 0 if the input date is invalid of if the input
       date is in the future.
     """
+    extra = 0
+    if year == 1:
+        extra = 15
     today = datetime.date.today()
     today = str(today)
     li = today.split("-")
     today_day = int(li[2])
     today_month = int(li[1])
     today_year = int(li[0])
-    total_days_in_birthday = days_between(year , month ,  day , today_year , today_month , today_day)
-    return total_days_in_birthday
-
-print(age_in_days(2001 , 12 , 28)) 
+    total_days_in_birthday = days_between(year, month,  day, today_year, today_month, today_day)
+    return total_days_in_birthday - extra
+ 
